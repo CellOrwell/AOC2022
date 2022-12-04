57478799ff0c41240734af2a13ebfc29b016307d
psyjm24 <psyjm24@nottingham.ac.uk>
2022-12-04 04:18:04 +0000

    Finished day 3 pt 2 code. ADDED fclose() to end of main script

--- day3/day3.c
+++ day3/day3.c
@@ -2,15 +2,16 @@
 #include <string.h>
 
 int getPriority(char);
-int getPriorityOfPackage(char *, int);
+int getPriorityOfPackage(char *, int, int, int);
 int isIn(char *, int, char);
 
 int main(int argc, char **argv)
 {
     FILE *file;
     char *fname = "day3.txt";
-    char buf[256];
-    int length, totPriority = 0;
+    char buf[256], groupStr[256];
+    int length, totPriority = 0, groupCount=0;
+    int pos[3];
 
     if(!(file=fopen(fname, "r")))
     {
@@ -20,29 +21,52 @@ int main(int argc, char **argv)
 
     while(fgets(buf, 256, file) != NULL)
     {
-        length = strlen(buf) - 1;
-        totPriority += getPriorityOfPackage(buf, length);
-        printf("%s, %d, %d\n", buf, length, getPriorityOfPackage(buf, length));
+        printf("%s", buf);
+        if(groupCount < 3)
+        {
+            if(groupCount == 0)
+            {
+                pos[0] = 0;
+            }
+            else
+            {
+                pos[groupCount] = strlen(groupStr) - 1;
+            }
+            groupStr[strlen(groupStr)-1] = '\0';
+            strcat(groupStr, buf);
+            groupCount++;
+        }
+        
+        if(groupCount == 3)
+        {
+            totPriority += getPriorityOfPackage(groupStr, pos[0], pos[1], pos[2]);
+            groupCount = 0;
+            strcpy(groupStr, "");
+        }
     }
 
     printf("Total priority = %d\n", totPriority);
+
+    fclose(file);
 }
 
-int getPriorityOfPackage(char str[256], int len)
+int getPriorityOfPackage(char str[256], int pos0, int pos1, int pos2)
 {
-    int i,j,mid,total = 0,brokeNum=0;
+    int i,j,k,mid,total = 0,brokeNum=0;
     char broke[256];
-    mid = len/2;
 
-    for(i=0; i<mid; i++)
+    for(i=0; i<pos1; i++)
     {
-        for(j=mid; j<len; j++)
+        for(j=pos1; j<pos2; j++)
         {
-            if(str[i] == str[j] && (isIn(broke, brokeNum, str[i]) == 1))
+            for(k=pos2; k<strlen(str); k++)
             {
-                total += getPriority(str[i]);
-                broke[brokeNum] = str[i];
-                brokeNum++;
+                if(str[i] == str[j] && str[j] == str[k] && (isIn(broke, brokeNum, str[i]) == 1))
+                {
+                    total += getPriority(str[i]);
+                    broke[brokeNum] = str[i];
+                    brokeNum++;
+                }
             }
         }
     }
